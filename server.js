4c6ef317a77e231c1f78131d95d3b2a0af8fb104
dev-raj-kumar <55322432+dev-raj-kumar@users.noreply.github.com>
2021-06-06 18:51:47 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const app = express();
+const server = require('http').Server(app);
+
+const io = new require('socket.io')(server);
+
+const {v4 : uuidv4} = require('uuid');
+
+const {ExpressPeerServer} = require('peer');
+const peerServer = ExpressPeerServer(server,{
+    debug : true
+});
+
+app.use('/peerjs',peerServer);
+app.set('view engine','ejs');
+app.use(express.static('public'));
+
+
+
+
+app.get('/:room', (req,res) => {
+    res.render('room',{roomId : req.params.room})
+})
+
+app.get('/',(req,res) => {
+    res.redirect(`/${uuidv4()}`);
+})
+
+
+io.on('connection', socket =>{
+    socket.on('join-room',(roomId,userId)=>{
+        socket.join(roomId);
+        socket.to(roomId).emit('user-connected',userId);
+        //console.log(roomId);
+        socket.on('message', message => {
+            io.to(roomId).emit('createMessage',message);
+        })
+    })
+})
+
+server.listen(3000);
